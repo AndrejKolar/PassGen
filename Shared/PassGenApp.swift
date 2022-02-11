458efd1c578aa5550d0ef0364ee42a055d71dbbc
Andrej Kolar <andrej.kolar@me.com>
2022-02-11 17:27:42 +0100

    Initial Commit

---
+++ Shared/PassGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassGenApp.swift
+//  Shared
+//
+//  Created by Andrej Kolar on 11.02.2022..
+//
+
+import SwiftUI
+
+@main
+struct PassGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
