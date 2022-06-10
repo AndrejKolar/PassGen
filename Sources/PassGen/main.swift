00813fc6483953068977428bcc647d369b0adb79
Andrej Kolar <andrej.kolar@me.com>
2022-06-10 17:26:37 +0200

    Setup all options

--- Sources/PassGen/main.swift
+++ Sources/PassGen/main.swift
@@ -1,11 +1,12 @@
-import ArgumentParser
 import Foundation
+import ArgumentParser
 
 struct PassGen: ParsableCommand {
     static let configuration =  CommandConfiguration(
-        abstract: "Generates a Random Password",
+        abstract: "Generates a Random Password.",
         version: "1.0.0",
-        subcommands: [Normal.self, Separated.self]
+        subcommands: [Normal.self, Separated.self],
+        defaultSubcommand: Normal.self
     )
 }
 
@@ -13,23 +14,56 @@ PassGen.main()
 
 extension PassGen {
     struct Normal: ParsableCommand {
-        static let configuration =  CommandConfiguration(abstract: "Generates a string password.", version: "1.0.0")
+        static let configuration =  CommandConfiguration(abstract: "Generates a string password. It uses uppercase, lowercase & number characters by default.", version: "1.0.0")
         
         @Option(name: .shortAndLong, help: "Length of the password.")
-        var length: Int = 16
+        var count: Int = 16
+        
+        @Flag(name: .shortAndLong, help: "Use number characters.")
+        var numbers = false
+        
+        @Flag(name: [.long, .customShort("p")], help: "Use uppercase characters.")
+        var uppercase = false
+
+        @Flag(name: [.long, .customShort("w")], help: "Use lowercase characters.")
+        var lowercase = false
+        
+        @Flag(name: .shortAndLong, help: "Use symbol characters.")
+        var symbols = false
         
         func validate() throws {
-            guard length >= 4 else {
+            guard count >= 4 else {
                 throw ValidationError("Password length must be at least 4.")
             }
         }
         
         mutating func run() throws {
+            var characters: Set<PasswordGenerator.CharacterType> = []
+            
+            if numbers {
+                characters.insert(.numbers)
+            }
+            
+            if uppercase {
+                characters.insert(.uppercase)
+            }
+            
+            if lowercase {
+                characters.insert(.lowercase)
+            }
+            
+            if symbols {
+                characters.insert(.symbols)
+            }
+            
+            if characters.count == 0 {
+                characters = [.lowercase, .uppercase, .numbers]
+            }
             
             let generator = PasswordGenerator()
             let password = generator.generate(
-                type: .normal(length),
-                characters: [.numbers, .lowercase, .uppercase]
+                type: .normal(count),
+                characters: characters
             )
             
             print("Generated password:")
@@ -38,7 +72,7 @@ extension PassGen {
     }
     
     struct Separated: ParsableCommand {
-        static let configuration =  CommandConfiguration(abstract: "Generates a password with separators.", version: "1.0.0")
+        static let configuration =  CommandConfiguration(abstract: "Generates a password with separators. It uses uppercase, lowercase & number characters by default. The default separator is a space.", version: "1.0.0")
         
         @Option(name: .shortAndLong, help: "Length of a single segment.")
         var length: Int = 8
@@ -58,6 +92,15 @@ extension PassGen {
         @Flag(name: .shortAndLong, help: "Use bar for separator.")
         var bar = false
         
+        @Flag(name: .shortAndLong, help: "Use number characters.")
+        var numbers = false
+
+        @Flag(name: [.long, .customShort("p")], help: "Use uppercase characters.")
+        var uppercase = false
+
+        @Flag(name: [.long, .customShort("w")], help: "Use lowercase characters.")
+        var lowercase = false
+        
         func validate() throws {
             guard length >= 3 else {
                 throw ValidationError("Word length must be at least 3.")
@@ -65,6 +108,12 @@ extension PassGen {
             
             guard count >= 2 else {
                 throw ValidationError("Word count must be at least 2.")
+            }
+            
+            let separatorFlagsOnCount = [dash, slash, underscore, bar].reduce(0) { $0 + ($1 ? 1 : 0) }
+            
+            if separatorFlagsOnCount > 1 {
+                throw ValidationError("Pass in only one separator flag.")
             }
         }
         
